7f4b1ca47ff6769d15fe06c651d763336289fdb9
David Gomez <dagomez@bu.edu>
2018-01-30 20:55:51 -0500

    updating label adder

--- API_Exercise/label_adder.py
+++ API_Exercise/label_adder.py
@@ -3,11 +3,11 @@ from PIL import ImageFont
 from PIL import ImageDraw 
 
 #tester function that adds a label to an image in the current directory
-def label_adder(image):
+def label_adder(image, label_text):
 	img = Image.open(str(image))
 	draw = ImageDraw.Draw(img)
 	# font = ImageFont.truetype(<font-file>, <font-size>)
 	font = ImageFont.truetype("arial.ttf", 20)
 	# draw.text((x, y),"Sample Text",(r,g,b))
-	draw.text((0, 0),"Sample Text",(255,255,255),font=font)
+	draw.text((0, 0), str(label_text) ,(255,255,255),font=font)
 	img.save(str(image))
