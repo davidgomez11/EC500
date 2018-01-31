dedefa2f56eb76d984d0c83da58d603b9ed1f361
David Gomez <dagomez@bu.edu>
2018-01-31 12:53:42 -0500

    updating file

--- API_Exercise/image_vid_tester.py
+++ API_Exercise/image_vid_tester.py
@@ -6,3 +6,6 @@ from PIL import Image
 def images_to_video():
 	fps= 1
 	subprocess.call(["ffmpeg","-y","-r",str(fps),"-i", "%*.jpg","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "video4.avi"])
+
+	#for windows users execute this line
+	#subprocess.call(["ffmpeg.exe","-y","-r",str(fps),"-i", "%*.jpg","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "video4.avi"])
