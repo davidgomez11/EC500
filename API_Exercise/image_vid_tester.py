f32f94424c8bd723f35e869fbdb430d168c812be
David Gomez <dagomez@bu.edu>
2018-02-05 22:02:15 -0500

    updating

--- API_Exercise/image_vid_tester.py
+++ API_Exercise/image_vid_tester.py
@@ -4,7 +4,7 @@ from PIL import Image
 #tester function that creates a video out of all the .jpg files in the current directory
 #this function feeds a ffmepg command to the command line tool 
 def images_to_video():
-	fps= 1
+	fps= 0.5
 	subprocess.call(["ffmpeg","-y","-r",str(fps),"-i", "%*.jpg","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "video4.avi"])
 
 	#for windows users execute this line
