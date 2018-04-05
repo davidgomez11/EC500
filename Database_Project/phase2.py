a53ac435eb509a6437302e9344a6ea07ccb9f624
David Gomez <dagomez@bu.edu>
2018-04-04 22:04:54 -0400

    updated

--- Database_Project/phase2.py
+++ Database_Project/phase2.py
@@ -34,6 +34,29 @@ def q_search(item):
 	for tweet in public_tweets:		#Searching through tweets generated
 
 		print(tweet.text)
+		print(tweet.created_at)
+		print(tweet.user.name)
+		print(tweet.user.screen_name)
+		print(tweet.user.location)
+		print(tweet.user.followers_count)
+		print(tweet.user.verified)
+		print(tweet.retweet_count)
+
+		dicto = {
+					"text" : tweet.text, 
+					"created_at" : tweet.created_at,
+					"name" : tweet.user.name,
+					"screen_name" : tweet.user.screen_name,
+					"location" : tweet.user.location,
+					"followers_count" : tweet.user.followers_count,
+					"verified" : tweet.user.verified,
+					"retweet_count" : tweet.retweet_count
+				}
+
+		#Here we insert the dictionary object into the MongoDB database
+		record.insert( dicto, check_keys=False )
+		
+		print("\n")
 
 def quick_search(item, directory):		
 
@@ -242,11 +265,18 @@ def main_run():
 
 	return dicto
 
+'''
 final_dict = {}
 
 final_dict = main_run()
 
 print(final_dict)
+'''
+
+
+item = raw_input("Please input something you want to search.\n ")
+
+q_search( item )
 
 
 
